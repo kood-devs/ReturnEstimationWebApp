9f08c3a5a87a3b6138a1a00fc2c621757b9559ef
kood-devs <64516150+kood-devs@users.noreply.github.com>
2020-05-04 00:11:04 +0900

    modify folder name

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'return_estimator.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'return_estimator.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
